f647a788c9db42328f86c1b2372efdef9a534180
Ki <ikyyy659@gmail.com>
2026-04-05 01:25:52 +0700

    Include Google Fonts in the app component
    Added Google Fonts link to Head component for styling.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import '@/styles/globals.css'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
