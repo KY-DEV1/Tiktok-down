2908a52040d843216b123e6f4688dc831e8bd6c3
Ki <ikyyy659@gmail.com>
2026-04-05 01:23:53 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import '@/styles/globals.css'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
 
 interface DownloadData {
   type: 'video' | 'image' | 'audio';
